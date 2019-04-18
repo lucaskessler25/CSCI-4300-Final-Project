52ff6e40d883e951f04a7a223190fab21f5dc8c9
Lucas Kessler <ljk30381@uga.edu>
2019-04-18 14:46:45 -0400

    Create editprofile-submit.php

---
+++ editprofile-submit.php
@@ -0,0 +1,8 @@
+<head>
+	<title>Field to Farm - Signup</title>
+	<meta charset="utf-8" />
+</head>
+
+<?php
+	#UPDATE userinfo SET description = 'Alfred Schmidt', name= 'Frankfurt' WHERE ID = :id;
+?>
